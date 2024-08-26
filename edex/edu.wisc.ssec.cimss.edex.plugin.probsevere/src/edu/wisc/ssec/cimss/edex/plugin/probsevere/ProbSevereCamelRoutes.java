7a624b621d8362f90bb90b5638449d4ef5cbda78
thomas.gurney <Thomas.Gurney@noaa.gov>
2024-08-26 17:04:42 -0500

    Issue #2037700 Move EDEXRouteBuilder to separate plugin (round 2)
    Package name fixes for commits that have happened since last time
    
    Change-Id: Ibf9cbdc714d4d70e7c16e96cde497baca70ef351

--- edex/edu.wisc.ssec.cimss.edex.plugin.probsevere/src/edu/wisc/ssec/cimss/edex/plugin/probsevere/ProbSevereCamelRoutes.java
+++ edex/edu.wisc.ssec.cimss.edex.plugin.probsevere/src/edu/wisc/ssec/cimss/edex/plugin/probsevere/ProbSevereCamelRoutes.java
@@ -20,7 +20,7 @@
 
 package edu.wisc.ssec.cimss.edex.plugin.probsevere;
 
-import com.raytheon.uf.edex.esb.camel.EDEXRouteBuilder;
+import com.raytheon.uf.edex.routes.EDEXRouteBuilder;
 
 /**
  * Camel routes converted from file "probsevere-ingest.xml", context "probsevere-camel"
